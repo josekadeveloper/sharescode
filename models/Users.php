a9b9f5756f9cb5ff68edfacbce844cd016dec39b
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2021-03-19 23:33:39 +0100

    Se crean los usuarios en la base de datos y se pueden registrar ellos mismos

--- models/Users.php
+++ models/Users.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "users".
@@ -12,11 +13,20 @@ use Yii;
  * @property string $password
  * @property bool $is_admin
  *
+ * @property Answer[] $answers
  * @property Portrait[] $portraits
+ * @property Query[] $queries
  * @property Reminder[] $reminders
  */
-class Users extends \yii\db\ActiveRecord
+class Users extends \yii\db\ActiveRecord implements IdentityInterface
 {
+    const SCENARIO_CREATE = 'create';
+    const SCENARIO_UPDATE = 'update';
+    const SCENARIO_REGISTER = 'register';
+
+    public $password_repeat;
+    public $reCaptcha;
+
     /**
      * {@inheritdoc}
      */
@@ -35,8 +45,17 @@ class Users extends \yii\db\ActiveRecord
             [['is_admin'], 'boolean'],
             [['nickname', 'password'], 'string', 'max' => 255],
             [['nickname'], 'unique'],
+            [['password', 'password_repeat'], 'required', 'on' => [self::SCENARIO_CREATE, self::SCENARIO_REGISTER]],
+            [['password'], 'compare', 'on' => [self::SCENARIO_CREATE, self::SCENARIO_UPDATE, self::SCENARIO_REGISTER]],
+            [['password_repeat'], 'safe', 'on' => [self::SCENARIO_UPDATE, self::SCENARIO_REGISTER]],
         ];
     }
+
+    public function attributes()
+    {
+        return array_merge(parent::attributes(), ['password_repeat']);
+    }
+
 
     /**
      * {@inheritdoc}
@@ -51,14 +70,105 @@ class Users extends \yii\db\ActiveRecord
         ];
     }
 
+    public function beforeSave($insert)
+    {
+        if (!parent::beforeSave($insert)) {
+            return false;
+        }
+
+        if ($insert) {
+            if ($this->scenario === self::SCENARIO_CREATE || self::SCENARIO_REGISTER) {
+                goto salto;
+            }
+        } else {
+            if ($this->scenario === self::SCENARIO_UPDATE) {
+                if ($this->password === '') {
+                    $this->password = $this->getOldAttribute('password');
+                } else {
+                    salto:
+                    $this->password = Yii::$app->security
+                        ->generatePasswordHash($this->password);
+                }
+            }
+        }
+        return true;
+    }
+
+    public function beforeDelete()
+    {
+        if (!parent::beforeDelete()) {
+            return false;
+        }
+
+        if ($this->getPortrait()->exists()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+    }
+
+    public function getAuthKey()
+    {
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+    }
+
+    public static function findByNickName($nickname)
+    {
+        return static::findOne(['nickname' => $nickname]);
+    }
+
+    public function validatePassword($password)
+    {
+        return Yii::$app->security
+            ->validatePassword($password, $this->password);
+    }
+
+    /**
+     * Gets query for [[Answers]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAnswers()
+    {
+        return $this->hasMany(Answer::class, ['us_id' => 'id']);
+    }
+
     /**
      * Gets query for [[Portraits]].
      *
      * @return \yii\db\ActiveQuery
      */
-    public function getPortraits()
+    public function getPortrait()
     {
-        return $this->hasMany(Portrait::class, ['us_id' => 'id'])->inverseOf('us');
+        return $this->hasOne(Portrait::class, ['us_id' => 'id'])
+                    ->inverseOf('users');
+    }
+
+    /**
+     * Gets query for [[Queries]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getQueries()
+    {
+        return $this->hasMany(Query::class, ['us_id' => 'id']);
     }
 
     /**
@@ -68,6 +178,6 @@ class Users extends \yii\db\ActiveRecord
      */
     public function getReminders()
     {
-        return $this->hasMany(Reminder::class, ['us_id' => 'id'])->inverseOf('us');
+        return $this->hasMany(Reminder::class, ['us_id' => 'id']);
     }
 }
---
+++ models/UsersSearch.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Users;
+
+/**
+ * UsersSearch represents the model behind the search form of `\app\models\Users`.
+ */
+class UsersSearch extends Users
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['nickname', 'password'], 'safe'],
+            [['is_admin'], 'boolean'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Users::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'is_admin' => $this->is_admin,
+        ]);
+
+        $query->andFilterWhere(['ilike', 'nickname', $this->nickname])
+            ->andFilterWhere(['ilike', 'password', $this->password]);
+
+        return $dataProvider;
+    }
+}
