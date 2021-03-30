8c73aaca84dbb6d900bd8075f63f16f7d1a02514
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2021-03-30 23:56:12 +0200

    Adaptando al creación de la respuesta a la query

--- views/answer/create.php
+++ views/answer/create.php
@@ -15,6 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $this->render('_form', [
         'model' => $model,
+        'id' => $id,
+        'portrait_id' => $portrait_id,
     ]) ?>
 
 </div>
