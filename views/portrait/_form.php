a46aacc628e270fdaf3849c2582bd5bed422311b
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2021-06-02 16:15:14 +0200

    Validación de formulario de crear de admin (sin cambio en la clase is-valid)

--- views/portrait/_form.php
+++ views/portrait/_form.php
@@ -2,6 +2,84 @@
 
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
+use yii\helpers\Url;
+
+$urlNickname = Url::to(['portrait/looking-for-nickname-ajax']);
+$urlEmail = Url::to(['portrait/looking-for-email-ajax']);
+$urlRepository = Url::to(['portrait/looking-for-repository-ajax']);
+
+$validation = <<<EOT
+    $('#portrait-nickname').blur(function (ev) {
+        var nickname = $(this).val();
+
+        $.ajax({
+            type: 'GET',
+            url: '$urlNickname',
+            data: {
+                nickname: nickname
+            }
+        })
+        .done(function (data) {
+            if (data.find) {
+                $('#nickname').show();
+                $('#nickname').html('Error: nickname is already in use');
+                $('#nickname').addClass('text-danger');
+                /*$('#portrait-nickname').removeClass('form-control is-valid');
+                $('#portrait-nickname').addClass('form-control is-invalid');*/
+            } else {
+                $('#nickname').html(data.nickname);
+                $('#nickname').hide();
+                /*$('#portrait-nickname').removeClass('form-control is-invalid');
+                $('#portrait-nickname').addClass('form-control is-valid');*/
+            }
+        });
+    });
+
+    $('#portrait-email').blur(function (ev) {
+        var email = $(this).val();
+
+        $.ajax({
+            type: 'GET',
+            url: '$urlEmail',
+            data: {
+                email: email
+            }
+        })
+        .done(function (data) {
+            if (data.find) {
+                $('#email').show();
+                $('#email').html('Error: email is already in use');
+                $('#email').addClass('text-danger');
+            } else {
+                $('#email').html(data.email);
+                $('#email').hide();
+            }
+        });
+    });
+
+    $('#portrait-repository').blur(function (ev) {
+        var repository = $(this).val();
+
+        $.ajax({
+            type: 'GET',
+            url: '$urlRepository',
+            data: {
+                repository: repository
+            }
+        })
+        .done(function (data) {
+            if (data.find) {
+                $('#repository').show();
+                $('#repository').html('Error: repository is already in use');
+                $('#repository').addClass('text-danger');
+            } else {
+                $('#repository').html(data.repository);
+                $('#repository').hide();
+            }
+        });
+    });
+EOT;
+$this->registerJs($validation);
 
 $sex = ['Men' => 'Men',
         'Woman' => 'Woman'];
@@ -17,6 +95,10 @@ $sex = ['Men' => 'Men',
 
             <?= $form->field($model, 'nickname')->textInput(['maxlength' => true]) ?>
 
+            <?= Html::label('', '', [
+                'id' => 'nickname',
+            ]) ?>
+
             <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true])->label('Password') ?>
                 
             <?= $form->field($model, 'password')->passwordInput(['maxlength' => true])->label('Password Repeat')  ?>
@@ -24,8 +106,16 @@ $sex = ['Men' => 'Men',
             <?= $form->field($model, 'date_register')->hiddenInput(['value' => date('Y-m-d H:i:s')])->label(false) ?>
 
             <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+            
+            <?= Html::label('', '', [
+                'id' => 'email',
+            ]) ?>
 
             <?= $form->field($model, 'repository')->textInput(['maxlength' => true]) ?>
+
+            <?= Html::label('', '', [
+                'id' => 'repository',
+            ]) ?>
 
             <?= $form->field($model, 'prestige_port')->hiddenInput(['maxlength' => true, 'value' => 'Initiate'])->label(false) ?>
 
