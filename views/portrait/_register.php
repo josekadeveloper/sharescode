7438e3471102b280d3b44e8b6f2e8a6306380fa8
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2021-06-02 11:16:58 +0200

    Insertado plugin de jQuery check-strength-password

--- views/portrait/_register.php
+++ views/portrait/_register.php
@@ -2,6 +2,18 @@
 
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
+
+$this->registerCssFile('@web/assets/checkPassword/password-strength.css', ['position' => $this::POS_HEAD]);
+$this->registerJsFile('@web/assets/checkPassword/password-strength.js', ['position' => $this::POS_END]);
+
+$checkPassword = <<<EOT
+$('#portrait-password_repeat').keyup(function(event) {
+    var password = $('#portrait-password_repeat').val();
+    checkPasswordStrength(password);
+  });
+EOT;
+$this->registerJs($checkPassword);
+
 $sex = ['Men' => 'Men',
         'Woman' => 'Woman'];
 ?>
@@ -25,10 +37,21 @@ $sex = ['Men' => 'Men',
         <?= $form->field($model, 'prestige_port')->hiddenInput(['maxlength' => true, 'value' => 'Initiate'])->label(false) ?>
 
         <?= $form->field($model, 'sex')->textInput(['maxlength' => true])->dropDownList($sex)  ?>
+        
+        <hr>
+        <ul class="pswd_info" id="passwordCriterion">
+            <li data-criterion="length" class="invalid">8-15 <strong>Characters</strong></li>
+            <li data-criterion="capital" class="invalid">At least <strong>one capital letter</strong></li>
+            <li data-criterion="small" class="invalid">At least <strong>one small letter</strong></li>
+            <li data-criterion="number" class="invalid">At least <strong>one number</strong></li>
+            <li data-criterion="special" class="invalid">At least <strong>one Specail Characters </strong></li>
+        </ul>
+        <div id="password-strength-status"></div>
+        <hr>
 
-            <div class="form-group">
-                <?= Html::submitButton('Register', ['class' => 'btn btn-success']) ?>
-            </div>
+        <div class="form-group">
+            <?= Html::submitButton('Register', ['class' => 'btn btn-success']) ?>
+        </div>
         <?php ActiveForm::end(); ?>
 
     </div>
