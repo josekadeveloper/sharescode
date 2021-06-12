8630bbc1bdf9b957b874167d4d23c8833c7cacb5
joseckk <josecarlos.delarosa@iesdonana.org>
2021-06-12 18:58:52 +0200

    Diseño y maquetación index + ventana modal

--- views/query/_post.php
+++ views/query/_post.php
@@ -627,8 +627,8 @@ if (!Yii::$app->user->isGuest) {
                     <?php endif ?>
                    <div id="modals-<?= $model->id ?>">
                         <?php if ($user_actually_id): ?>
-                            <div class="modal fade" id="ex-<?= $answer->id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
+                            <div class="modal fullscreen-modal fade modal-lg" id="ex-<?= $answer->id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
